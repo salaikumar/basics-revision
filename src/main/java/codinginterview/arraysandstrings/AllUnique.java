01b07d34c9fa2d77a4674d62d366b9cb962fab6c
Salaikumar <saravanan1115@gmail.com>
2016-02-07 09:11:17 +0530

    Cracking the Coding Interview Solutions

---
+++ src/main/java/codinginterview/arraysandstrings/AllUnique.java
@@ -0,0 +1,12 @@
+package codinginterview.arraysandstrings;
+
+/**
+ *Implement an algorithm to determine if a string has all unique characters. What if you
+ can not use additional data structures?
+ */
+public class AllUnique {
+
+    public boolean isAllUnique(String input){
+        return false;
+    }
+}
